39e9692d172b2210928bdf572a04d4de6644fdec
Lee chung <cs00327@naver.com>
2020-11-18 01:43:01 +0900

    2020-11-18 1:42 second commit

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) # 문자, 200글자
+    pub_date = models.DateTimeField('date published')  # 날짜와 시간 필드
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question =  models.ForeignKey(Question, on_delete=models.CASCADE)  # 외래키 
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
