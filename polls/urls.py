2b5f1c16c3c0500842d5c498b04b0c6d1ca77cd4
Lee chung <cs00327@naver.com>
2020-11-19 00:47:02 +0900

    2020-11-19 12:46 tutorial part 3 view 사용 Html 불러오기 및 연결

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 from . import  views
 
-urlpatterns = [path('',views.index , name='index'),  ## 이름 index인 함수 호출
 
-               ]
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+] ## 이름 index인 함수 호출
+
+# <int:question_id>/vote/ django의 url 패턴 예로 《/polls/34/》를 요청했다고 하면,
+# Django는 mysite.urls 파이썬 모듈을 불러오게 됩니다. ROOT_URLCONF 설정에 의해
+# 해당 모듈을 바라보도록 지정되어 있기 때문입니다. mysite.urls에서 urlpatterns라는
+# 변수를 찾고, 순서대로 패턴을 따라갑니다. 'polls/'를 찾은 후엔, 일치하는 텍스트("polls/")를
+# 버리고, 남은 텍스트인 "34/"를 〈polls.urls〉 URLconf로 전달하여 남은 처리를 진행합니다.
+# 거기에 '<int:question_id>/'와 일치
 # 127.0.0.1/polls/ 를 받고 mysite/urls에서 여기로 보내준다음  views로 이동시켜줌
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,50 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+from django.template import loader
 from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
 
 def index(request):
-    return HttpResponse("Hello the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,  # context를 통해 데이터를 전달
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)  v1
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")   ## 404 예외처리 기능 , raise 예외 발생시키기 (메시지) v2
+#     return render(request,'polls/detail.html',{'question':question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)   ##  try catch 문 생략 가능 get_object_or_404
+    return render(request, 'polls/detail.html', {'question': question})  # v3
+#get_object_or_404() 함수는 Django 모델을 첫번째 인자로 받고,
+# 몇개의 키워드 인수를 모델 관리자의 get() 함수에 넘깁니다.
+# 만약 객체가 존재하지 않을 경우, Http404 예외가 발생합니다.
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+## view에서는 request 라는 인자를 받고  HttpResponse 로 리턴함
+## 클라이언트에서 request를 받으면 여러가지 정보가 들어있음
